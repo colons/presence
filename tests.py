b545babd3e3965e7d1feb98ffef628a2e8fa998e
Iain Dawson <iain@colons.co>
2014-04-12 14:18:49 +0100

    Reimplement pingdom.

---
+++ tests.py
@@ -0,0 +1,30 @@
+import requests
+from unittest2 import TestCase
+
+
+class PresenceTest(TestCase):
+    """
+    Make sure everything is still up.
+    """
+
+
+for url in [
+    'http://colons.co/',
+    'https://colons.co/',
+    'http://nkd.su/',
+    'https://nkd.su/',
+    'http://musicfortheblind.co.uk/',
+    'http://hotscoops.co/',
+    'http://sanityvalve.com/',
+    'http://bldm.us/',
+]:
+    def check(self):
+        requests.get(url)
+
+    name = 'test_{url}'.format(**locals())
+    check.__name__ = name
+
+    setattr(PresenceTest, name, check)
+
+
+check = None  # fukken python scopes
