827ece00f404d11b7f0074f6ac21518dc1837a77
Iain Dawson <iain@colons.co>
2015-01-18 11:12:05 +0000

    Make sure the <title>s are what we think they are and check more sites.

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
-import requests
 from unittest2 import TestCase
+
+from bs4 import BeautifulSoup
+import requests
 
 
 class PresenceTest(TestCase):
@@ -7,26 +9,75 @@ class PresenceTest(TestCase):
     Make sure everything is still up.
     """
 
+    @classmethod
+    def add_check(cls, url, expected_title):
+        def check(self):
+            got_title, = [
+                e.text for e in
+                BeautifulSoup(requests.get(url).text).select('title')
+            ]
+            self.assertEqual(
+                got_title.strip(), expected_title,
+                '{url} is bad\n{got!r} != {exp!r})'.format(
+                    url=url, got=got_title.strip(), exp=expected_title,
+                )
+            )
 
-for url in [
-    'http://colons.co/',
-    'https://colons.co/',
-    'http://nkd.su/',
-    'https://nkd.su/',
-    'http://musicfortheblind.co.uk/',
-    'http://hotscoops.co/',
-    'http://sanityvalve.com/',
-    'http://bldm.us/',
-    'http://giantasshole.com/',
-    'http://nivi.org.uk/',
+        name = 'test_{url}_{title}'.format(url=url, title=expected_title)
+        check.__name__ = name
+
+        setattr(cls, name, check)
+
+
+for expected_title, urls in [
+    ('a neko desu request robot | nkd.su', [
+        'http://nkd.su/',
+        'https://nkd.su/',
+        'http://www.nkd.su/',
+    ]),
+    ('Barry Watson - Raconteur & Musician', [
+        'http://barrywatson.co/',
+        'http://www.barrywatson.co/',
+    ]),
+    ('Home | Music for the Blind', [
+        'http://musicfortheblind.co.uk/',
+        'http://www.musicfortheblind.co.uk/',
+        'http://bldm.us/',
+        'http://www.bldm.us/',
+    ]),
+    ('i am a cool internet person', [
+        'http://colons.co/',
+        'https://colons.co/',
+        'http://www.colons.co/',
+        'https://www.colons.co/',
+        'http://nivi.org.uk/',
+        'http://www.nivi.org.uk/',
+        'http://dump.musicfortheblind.co.uk/',
+    ]),
+    ('irc.biz.ru:6697', [
+        'http://ggj14.colons.co/',
+    ]),
+    ('Music | Sanity Valve', [
+        'http://sanityvalve.com/',
+        'http://www.sanityvalve.com/',
+    ]),
+    ('The Music for the Blind Review', [
+        'http://opinions.colons.co/',
+        'http://opinions.musicfortheblind.co.uk/',
+        'http://opinions.bldm.us/',
+    ]),
+    ('trenchfoot', [
+        'http://woof.colons.co/',
+    ]),
+    ('The Latest Scoops!', [
+        'http://hotscoops.co/',
+        'http://www.hotscoops.co/',
+        'https://hotscoops.co/',
+    ]),
+    ("Vinny's profile", [
+        'http://giantasshole.com/',
+        'http://www.giantasshole.com/',
+    ]),
 ]:
-    def check(self):
-        requests.get(url)
-
-    name = 'test_{url}'.format(**locals())
-    check.__name__ = name
-
-    setattr(PresenceTest, name, check)
-
-
-check = None  # fukken python scopes
+    for url in urls:
+        PresenceTest.add_check(url, expected_title)
